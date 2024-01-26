69810d32c83f5b8997de1fc979715064d6757fc7
haticebabaoglu <haticebaba63@gmail.com>
2024-01-26 23:56:48 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import './App.css'
 
+const cardImages = [
+  { "src": "/img/helmet-1.png" },
+  { "src": "/img/potion-1.png" },
+  { "src": "/img/ring-1.png" },
+  { "src": "/img/scroll-1.png" },
+  { "src": "/img/shield-1.png" },
+  { "src": "/img/sword-1.png" }
+
+]
+
+// shuffle card
+const shuffleCards = () => {
+  
+}
+
 function App() {
   return (
     <div className="App">
