30d223da5dd220566495207a2caf4515330d1911
haticebabaoglu <haticebaba63@gmail.com>
2024-01-29 13:37:58 +0300

    .

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,12 @@
 import './SingleCard.css'
 
-export default function SingleCard({card, handleChoice}) {
+export default function SingleCard({card, handleChoice, flipped, disabled}) {
   const handleClick = () => {
 
   }
 return(
     <div className='card' key={card.id}>
-           <div> 
+           <div className={flipped ? "flipped" : ""}>
               <img className='front' src={card.src}/>
               <img className='back' src='/img/cover.png'
                onClick={handleClick}/>
